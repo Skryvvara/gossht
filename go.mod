91f57d5a239ab21ef81fa4f3468b7e204e52efe8
Skryvvara <darkyne.gaming@outlook.de>
2024-07-17 14:54:57 +0200

    fix: ssh pty should behave almost normally now

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gdamore/tcell/v2 v2.7.1
 	github.com/rivo/tview v0.0.0-20240625185742-b0a7293b8130
 	golang.org/x/crypto v0.25.0
+	golang.org/x/term v0.22.0
 )
 
 require (
@@ -14,6 +15,5 @@ require (
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/term v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 )
--- internal/ssh/ssh.go
+++ internal/ssh/ssh.go
@@ -12,27 +12,38 @@ import (
 	"time"
 
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
 	"golang.org/x/crypto/ssh/knownhosts"
+	"golang.org/x/term"
 )
+
+func SSHAgent() ssh.AuthMethod {
+	if sshAgent, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
+		return ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers)
+	}
+	return nil
+}
 
 func SSHConnect(host, user string) {
 	sshPath := path.Join(os.Getenv("HOME"), ".ssh")
 	//TODO: support other keys than id_rsa and custom paths
-	keyPath := path.Join(sshPath, "id_rsa")
+	//keyPath := path.Join(sshPath, "id_rsa")
 
 	var err error
-	var signer ssh.Signer
+	//var signer ssh.Signer
 
 	// Read the private key file
-	pKey, err := os.ReadFile(keyPath)
-	if err != nil {
-		log.Fatalf("unable to read private key: %v", err)
-	}
+	/*
+		pKey, err := os.ReadFile(keyPath)
+		if err != nil {
+			log.Fatalf("unable to read private key: %v", err)
+		}*/
 
-	signer, err = ssh.ParsePrivateKey(pKey)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
+	/*
+		signer, err = ssh.ParsePrivateKey(pKey)
+		if err != nil {
+			fmt.Println(err.Error())
+		}*/
 
 	var hostkeyCallback ssh.HostKeyCallback
 	hostkeyCallback, err = knownhosts.New(path.Join(sshPath, "known_hosts"))
@@ -44,7 +55,7 @@ func SSHConnect(host, user string) {
 		User:            user,
 		HostKeyCallback: hostkeyCallback,
 		Auth: []ssh.AuthMethod{
-			ssh.PublicKeys(signer),
+			SSHAgent(),
 		},
 		Timeout: time.Millisecond * 1000,
 	}
@@ -67,16 +78,35 @@ func SSHConnect(host, user string) {
 	}
 	defer session.Close()
 
+	// Get the terminal file descriptor
+	fd := int(os.Stdin.Fd())
+
+	// Put the terminal into raw mode
+	oldState, err := term.MakeRaw(fd)
+	if err != nil {
+		log.Fatalf("Failed to set terminal to raw mode: %v", err)
+	}
+	defer term.Restore(fd, oldState)
+
+	// Get the terminal size
+	width, height, err := term.GetSize(fd)
+	if err != nil {
+		log.Fatalf("Failed to get terminal size: %v", err)
+	}
+
 	modes := ssh.TerminalModes{
-		ssh.ECHO:          0,     // disable echoing
+		ssh.ECHO:          1,
+		ssh.ECHOCTL:       0,
 		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
 		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
 	}
-	if err := session.RequestPty("linux", 80, 40, modes); err != nil {
-		log.Fatal("request for pseudo terminal failed: ", err)
+
+	// Request a pseudo-terminal
+	if err := session.RequestPty("xterm-256color", height, width, modes); err != nil {
+		log.Fatalf("Request for pseudo terminal failed: %v", err)
 	}
 
-	//set input and output
+	// Set input and output
 	session.Stdout = os.Stdout
 	session.Stdin = os.Stdin
 	session.Stderr = os.Stderr
@@ -85,7 +115,17 @@ func SSHConnect(host, user string) {
 		log.Fatal("failed to start shell: ", err)
 	}
 
-	// Handle signals for termination
+	// Handle terminal resizing
+	go func() {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, syscall.SIGWINCH)
+		for range sigCh {
+			width, height, _ := term.GetSize(fd)
+			session.WindowChange(height, width)
+		}
+	}()
+
+	// Handle termination signals
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
 
